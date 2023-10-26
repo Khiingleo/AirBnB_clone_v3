4f41d01dc473e99f685227239b3db091bc779eaf
Shalom Ogoziem <shalomzymike@gmail.com>
2023-10-26 17:12:30 +0000

    fix importing error

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-from storage import models
+from models import storage
 from api.v1.views import app_views
 from flask import Flask, jsonify, make_response
 from os import getenv
