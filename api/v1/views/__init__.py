2fd51d0dfe4ca5f51cb38e8626468da6a8f76baa
PeterEkwere <peterekwere04@gmail.com>
2023-10-27 14:49:36 -0700

    created the amenities endpoint

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -5,5 +5,6 @@ from models import storage
 from models.state import State
 
 from api.v1.views.cities import *
+from api.v1.views.amenities import *
 from api.v1.views.index import *
 from api.v1.views.states import *
