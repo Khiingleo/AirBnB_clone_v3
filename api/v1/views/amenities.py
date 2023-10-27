2adb7c9b38740649eda7f05a60c6b5c875d02490
PeterEkwere <peterekwere04@gmail.com>
2023-10-27 14:56:19 -0700

    arranged amenities import alphabetically

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -3,12 +3,10 @@
     THis Module handles all default RESTFul API actions for amenities
     Author: Peter Ekwere
 """
-from models import storage
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
 from api.v1.views import app_views
 from flask import abort, jsonify, request
+from models import storage
+from models.amenity import Amenity
 
 
 @app_views.route('/amenities', methods=['GET'],
