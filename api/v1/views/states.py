7bbe9cb13922b871bcc2d9e3ac12146bc4d63c50
PeterEkwere <peterekwere04@gmail.com>
2023-10-27 09:36:26 -0700

    fixed delete operation for states

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 """ restful api for the State class """
 
-from flask import Flask, jsonify, request, abort
+from flask import jsonify, request, abort
 from models import storage
 from api.v1.views import app_views
 from models.state import State
-
 
 
 @app_views.route("/states", methods=['GET'], strict_slashes=False)
@@ -62,7 +61,7 @@ def update_state(states_id):
     if not request.get_json():
         return jsonify({"error": "Not a JSON"}), 400
     state_obj = storage.get(State, states_id)
-    if obj is None:
+    if state_obj is None:
         abort(404)
     obj_data = request.get_json()
     state_obj.name = obj_data['name']
